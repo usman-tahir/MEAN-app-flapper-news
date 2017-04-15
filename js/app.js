0e73b8bf2f9965c155640f164196544dfdcb4799
usman-tahir <tahir.usman.ali94@outlook.com>
2017-04-15 11:36:31 -0400

    added a basic posts templates sorted by upvotes

--- js/app.js
+++ js/app.js
@@ -6,5 +6,11 @@
   // Connect the app to its controller, and use scoping to access 'test'
   app.controller('MainCtrl', ['$scope', function ($scope) {
     $scope.test = 'Hello, World!';
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 4},
+      {title: 'post 3', upvotes: 2},
+      {title: 'post 4', upvotes: 2}
+    ];
   }]);
 }());
