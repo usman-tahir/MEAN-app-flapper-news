e4915816a3dfb7e9f3ed3092156d3ba159d065cd
usman-tahir <tahir.usman.ali94@outlook.com>
2017-04-15 11:51:44 -0400

    added a post creation form

--- js/app.js
+++ js/app.js
@@ -3,14 +3,13 @@
 
   var app = angular.module('flapperNews', []);
 
-  // Connect the app to its controller, and use scoping to access 'test'
+  // Connect the app to its controller
   app.controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.test = 'Hello, World!';
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 4},
-      {title: 'post 3', upvotes: 2},
-      {title: 'post 4', upvotes: 2}
-    ];
+    $scope.posts = [];
+
+    $scope.addPost = function () {
+      $scope.posts.push({title: $scope.title || 'A new post', upvotes: 0});
+      $scope.title = ''; // Clear the title
+    };
   }]);
 }());
