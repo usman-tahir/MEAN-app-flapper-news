c7d368003153a4d00597ea5858b73ea92e82d2ff
usman-tahir <tahir.usman.ali94@outlook.com>
2017-04-15 12:04:47 -0400

    added a basic upvote function to increase post upvotes

--- js/app.js
+++ js/app.js
@@ -11,5 +11,9 @@
       $scope.posts.push({title: $scope.title || 'A new post', upvotes: 0});
       $scope.title = ''; // Clear the title
     };
+
+    $scope.incrementUpvotes = function (post) {
+      post.upvotes += 1;
+    };
   }]);
 }());
